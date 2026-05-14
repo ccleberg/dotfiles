01978e84e15d6498dabb8ad58283dc0949729f25
Christian Cleberg <hello@cleberg.net>
2026-05-14 15:57:54 -0500

    update macos motd

--- macos/motd/motd.sh
+++ macos/motd/motd.sh
@@ -3,7 +3,7 @@ DIM='\033[2m'
 RESET='\033[0m'
 
 VPN=$(mullvad status 2>/dev/null | head -1 || echo "disconnected")
-IP=$(curl -s ifconfig.pro)
+IP=$(mullvad status | rg -o '(\d{1,3}\.){3}\d{1,3}')
 
 printf "\n"
 printf "  ${BOLD}%s${RESET}      ${DIM}%s${RESET}\n" "$USER" "$(uname -sr)"
